cf1c61752ee5d0050f4506d1c64e442e1b17220d
Emilly Nunes <emmilly.ti@gmail.com>
2025-03-31 16:39:14 -0400

    ok

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const colors = ["#E0F7FA", "#0288D1", "#01579B"];
+const colors = ["#fff9f7", "#fefff7", "#f7fff7"]; // grey, yellow, green
+
 const userName = prompt("What is your name?");
-const colorChoice = prompt("Pick a background color: 0 for light blue, 1 for medium blue, 2 for dark blue");
+const colorChoice = prompt("Pick a background color: 0 for grey, 1 for yellow, 2 for green");
 
 const userInfo = {
     name: userName,
     selectedColor: colors[colorChoice]
 };
 
-document.body.style.backgroundColor = userInfo.selectedColor;
+console.log("The first color in the array is:", colors[0]); // should
